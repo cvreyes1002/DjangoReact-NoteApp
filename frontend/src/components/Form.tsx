3ee983fd3488afedeade8011f536b7326bc3f366
C.Reyes <cvreyes@hotmail.com>
2026-06-17 09:20:36 +0800

    finish register, login

---
+++ frontend/src/components/Form.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react"
+import api from "../api"
+import { useNavigate } from "react-router-dom"
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants"
+import "../styles/Form.css"
+
+/*
+ route: route where we go to when we submit the form (token route or register route)
+ method: tell us if we're registering or logging in
+ */
+function Form({ route, method }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const name = method === "login" ? "Login" : "Register";
+
+  const handleSubmit = async (e) => {
+    setLoading(true) 
+    // Calling e.preventDefault() tells the browser: "Hey, don't do your default behavior.
+    // Let me handle this submission manually with JavaScript instead."
+    // This is crucial in modern web apps (like React or Vue) where you want to send data
+    // to an API in the background without making the whole page reload.
+    e.preventDefault();
+
+    try {
+      const res = await api.post(route, {username, password} )
+      if (method === "login") {
+        localStorage.setItem(ACCESS_TOKEN, res.data.access)
+        localStorage.setItem(REFRESH_TOKEN, res.data.refresh)
+        navigate("/")
+      } else {
+        navigate("/login")
+      }
+     }
+    catch (error) {
+      alert(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <h1>{name}</h1>
+      <input
+        className="form-input"
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+      />
+      <input
+        className="form-input"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <button className="form-button" type="submit">
+        {name}
+      </button>
+    </form>
+  );
+}
+
+export default Form
