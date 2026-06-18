31fff7e22981f3c9948c66ad5f0d361f893f32cd
C.Reyes <cvreyes@hotmail.com>
2026-06-18 08:36:28 +0800

    finish frontend and backend

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react"
-import api from "../api"
-import { useNavigate } from "react-router-dom"
-import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants"
-import "../styles/Form.css"
+import { useState } from "react";
+import api from "../api";
+import { useNavigate } from "react-router-dom";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+import "../styles/Form.css";
+import LoadingIndicator from "./LoadingIndicator";
 
 /*
  route: route where we go to when we submit the form (token route or register route)
@@ -17,7 +18,7 @@ function Form({ route, method }) {
   const name = method === "login" ? "Login" : "Register";
 
   const handleSubmit = async (e) => {
-    setLoading(true) 
+    setLoading(true);
     // Calling e.preventDefault() tells the browser: "Hey, don't do your default behavior.
     // Let me handle this submission manually with JavaScript instead."
     // This is crucial in modern web apps (like React or Vue) where you want to send data
@@ -25,21 +26,20 @@ function Form({ route, method }) {
     e.preventDefault();
 
     try {
-      const res = await api.post(route, {username, password} )
+      const res = await api.post(route, { username, password });
       if (method === "login") {
-        localStorage.setItem(ACCESS_TOKEN, res.data.access)
-        localStorage.setItem(REFRESH_TOKEN, res.data.refresh)
-        navigate("/")
+        localStorage.setItem(ACCESS_TOKEN, res.data.access);
+        localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+        navigate("/");
       } else {
-        navigate("/login")
+        navigate("/login");
       }
-     }
-    catch (error) {
-      alert(error)
+    } catch (error) {
+      alert(error);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit} className="form-container">
@@ -58,6 +58,7 @@ function Form({ route, method }) {
         onChange={(e) => setPassword(e.target.value)}
         placeholder="Password"
       />
+      {loading && <LoadingIndicator />}
       <button className="form-button" type="submit">
         {name}
       </button>
@@ -65,4 +66,4 @@ function Form({ route, method }) {
   );
 }
 
-export default Form
+export default Form;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import api from "../api";
-import { data } from "react-router-dom";
+import Note from "../components/Note";
+import "../styles/Home.css"
+// import { data } from "react-router-dom";
 
 function Home() {
   const [notes, setNotes] = useState([]);
@@ -15,11 +17,70 @@ function Home() {
     api
       .get("/api/notes/") // Sends an HTTP GET request to the /api/notes/ endpoint to retrieve the notes.
       .then((res) => res.data) // Extracts the JSON data from the server's response.
-      .then((data) => { setNotes(data); console.log(data)}) // Updates your React state by passing the retrieved data into the setNotes function, which triggers a re-render to display the notes.
+      .then((data) => {
+        setNotes(data);
+        console.log(data);
+      }) // Updates your React state by passing the retrieved data into the setNotes function, which triggers a re-render to display the notes.
       .catch((err) => alert(err)); // Catches any errors that occur during the request (e.g., server issues, network failures) and pops up an alert box showing the error.
   };
 
-  return <div>Home</div>;
+  const deleteNote = (id) => {
+    api
+      .delete(`/api/notes/delete/${id}/`)
+      .then((res) => {
+        if (res.status === 204) alert("Note deleted.");
+        else alert("Failed to delete note.");
+        getNotes();
+      })
+      .catch((error) => alert(error));
+  };
+
+  const createNote = (e) => {
+    e.preventDefault();
+    api
+      .post("/api/notes/", { content, title })
+      .then((res) => {
+        if (res.status === 201) alert("Note created.");
+        else alert("Failed to make note.");
+        getNotes();
+      })
+      .catch((err) => alert(err));
+  };
+
+  return (
+    <div>
+      <div>
+        <h2>Notes</h2>
+        {notes.map((note) => (
+          <Note note={note} onDelete={deleteNote} key={note.id} />
+        ))}
+      </div>
+      <h2>Create a Note</h2>
+      <form onSubmit={createNote}>
+        <label htmlFor="title">Title:</label>
+        <br />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <label htmlFor="content">Content:</label>
+        <br />
+        <textarea
+          name="content"
+          id="content"
+          required
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
 
 export default Home;
